513a76d124b50a2c6957578bee3f3e1dbb81b78c
Theneyre <163881074+TheNeyre@users.noreply.github.com>
2024-06-15 16:19:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ class Config:
 	token = 'your token'
 	req_channel_id = 'АЙДИ Канала с формой и кнопкой'
 	req_result_channel_id = 'АЙДИ Канала, куда отправляются реквесты'
+	REQ_Moderator_ID = 'Айди модератора по реквестам'
